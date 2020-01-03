08028379b1c209ca81819a6d52e2dd1eb69da5ed
Gulyás Simone <gulyassimone@gmail.com>
2020-01-03 19:05:13 +0100

    Vegleges

--- assignment_plus/main.cpp
+++ assignment_plus/main.cpp
@@ -3,106 +3,174 @@
 
 using namespace std;
 
-void szomszed_nullazas(int resztvevok_szama, long long szomszed[]);
 void leult_nullazas(int resztvevok_szama, int leult[]);
-void kiirVoltSzomszedE(int resztvevok_szama, long long *szomszed);
-void kiirLeultE(int resztvevok_szama, int *leult);
-bool voltSzomszedE(long long szomszed, int uj_szomszed);
+void szomszed_nullazas(int resztvevok_szama, int szomszed[]);
+
 bool leultE(int leult[], int poz, int leulo);
-void kereses(int resztvevok_szama,long long szomszed[], int leult[], int i, int j);
-void ulesi_sorrend(int resztvevok_szama, long long *szomszed, int *leult);
+void kereses(int resztvevok_szama,int szomszed[], int leult[], int i, int j, int db);
+void ulesi_sorrend(int resztvevok_szama, int *szomszed, int *leult);
+void kiirVoltSzomszedE(int resztvevok_szama, int *szomszed);
+void kiirLeultE(int resztvevok_szama, int *leult);
+bool OsszesSzomszedMegvan(int resztvevok_szama, int szomszed[]);
 
 int main()
 {
     int resztvevok_szama;
     cin >> resztvevok_szama;
-    long long szomszed[resztvevok_szama];
+    int szomszed[resztvevok_szama*resztvevok_szama];
     int leult[resztvevok_szama];
+
+
+    leult_nullazas(resztvevok_szama, leult);
     szomszed_nullazas(resztvevok_szama, szomszed);
-    int i=0;
-    while(i<resztvevok_szama/2)
+
+    kiirLeultE(resztvevok_szama, leult);
+    kiirVoltSzomszedE(resztvevok_szama, szomszed);
+    for(int i=0; OsszesSzomszedMegvan(resztvevok_szama, szomszed); i++)
     {
-        cout<< "i erteke "<< i<< endl;
         leult_nullazas(resztvevok_szama, leult);
-        kereses(resztvevok_szama, szomszed, leult, 0,0);
-        ulesi_sorrend(resztvevok_szama, szomszed, leult);
-        kiirLeultE(resztvevok_szama, leult);
-        kiirVoltSzomszedE(resztvevok_szama, szomszed);
-        i++;
+        kereses( resztvevok_szama,szomszed, leult,0, 0,1);
+        cout << "ennyi megoldasa van" <<i+1<<endl;
     }
+
     kiirVoltSzomszedE(resztvevok_szama, szomszed);
     kiirLeultE(resztvevok_szama, leult);
 
     return 0;
 }
 
-void kereses(int resztvevok_szama,long long szomszed[], int leult[],int i,int j)
+void kereses(int resztvevok_szama,int szomszed[], int leult[],int i, int j, int db)
 {
-    while(i<resztvevok_szama)
+    leult[db]=-1;
+    cout << "ujabb"<<endl;
+    kiirLeultE(resztvevok_szama, leult);
+    while(j<resztvevok_szama && db<resztvevok_szama)
     {
-        while(j<resztvevok_szama)
+        cout << "i es j erteke " << i << " " << j << "db "<< db << endl;
+        if(!leultE(leult, resztvevok_szama, j))
         {
-
-            if(!leultE(leult, i, j))
+            cout << "j nem ult meg e " << j  << endl;
+            if(szomszed[j+i*resztvevok_szama]==1)
             {
-                if(!voltSzomszedE(szomszed[i], j))
-                {
-                    leult[i]=j;
-                    j=resztvevok_szama;
-                    cout << "probalkozas " << endl;
-                    kiirLeultE(resztvevok_szama, leult);
-                }
+                cout << "leulhet " << j  << endl;
+
+                leult[db]=j;
+                db++;
+                szomszed[j+i*resztvevok_szama]=0;
+                szomszed[j*resztvevok_szama+i]=0;
+                i=j;
+                j=0;
+                kiirVoltSzomszedE(resztvevok_szama, szomszed);
+                kiirLeultE(resztvevok_szama, leult);
             }
-            j++;
+            else
+            {
+                cout << "de a "<< i << "melle nem ultetheto" << endl;
+                kiirLeultE(resztvevok_szama, leult);
+                j++;
+            }
+
         }
-        if(leult[i]==-1)
+        else
         {
-            kereses(resztvevok_szama, szomszed, leult, i-1, leult[i-1]+1);
+            ++j;
+
         }
-        j=0;
-        i++;
     }
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    if(j==resztvevok_szama && db==resztvevok_szama-1 && resztvevok_szama%2==0)
+    {
+        for(j=0; j<resztvevok_szama; j++)
+        {
+            cout << "i es j erteke " << i << " " << j << endl;
+            if(!leultE(leult, resztvevok_szama, j))
+            {
+                cout << "akkor ot leultetem ide" << endl;
+                leult[db]=j;
+                db++;
+                szomszed[j+i*resztvevok_szama]=0;
+                szomszed[j*resztvevok_szama+i]=0;
+                i=j;
+                kiirVoltSzomszedE(resztvevok_szama, szomszed);
+                kiirLeultE(resztvevok_szama, leult);
+            }
+        }
+    }
+    else if(j==resztvevok_szama && db!=resztvevok_szama)
+    {
+
+        szomszed[leult[db-2]+(db-1)*resztvevok_szama]=1;
+        szomszed[leult[db-1]*resztvevok_szama+(db-2)]=1;
+        cout << db << " db  " <<  leult[db-2]<<" leult-1 "<< leult[db-1]<< " leult " << endl;
+        kereses(resztvevok_szama, szomszed, leult, leult[db-2], leult[db-1]+1, db-1);
+    }
+
+    szomszed[0+i*resztvevok_szama]=0;
+    szomszed[0+i]=0;
+    db++;
+    cout << "utcso" << endl;
+    kiirVoltSzomszedE(resztvevok_szama, szomszed);
 }
 
-void szomszed_nullazas(int resztvevok_szama, long long szomszed[])
+bool OsszesSzomszedMegvan(int resztvevok_szama, int szomszed[])
+{
+    bool van=false;
+    for(int i=0; i<resztvevok_szama; i++)
+    {
+        for(int j=0; j<resztvevok_szama; j++)
+        {
+            if(szomszed[i*resztvevok_szama+j]==1)
+            {
+                van=true;
+            }
+
+        }
+    }
+    return van;
+}
+
+void szomszed_nullazas(int resztvevok_szama, int szomszed[])
 {
     for(int i=0; i<resztvevok_szama; i++)
     {
-        szomszed[i]=pow(2,i);
+        for(int j=0; j<resztvevok_szama; j++)
+        {
+            if(i!=j)
+            {
+                szomszed[i*resztvevok_szama+j]=1;
+            }
+            else
+            {
+                szomszed[i*resztvevok_szama+j]=0;
+            }
+
+        }
     }
 }
 void leult_nullazas(int resztvevok_szama, int leult[])
 {
     for(int i=0; i<resztvevok_szama; i++)
     {
-        leult[i]=-1;
-    }
-}
-
-bool voltSzomszedE(long long szomszed, int uj_szomszed)
-{
-    bool voltSzomszed=false;
-    int seged_szomszed=szomszed;
-    int rownum=0;
-    while(!voltSzomszed && seged_szomszed>0)
-    {
-        if(seged_szomszed%2>0)
+        if(i!=0)
         {
-            if(pow(2,rownum)==pow(2,uj_szomszed))
-            {
-                voltSzomszed=true;
-            }
+            leult[i]=-1;
         }
-        seged_szomszed=seged_szomszed/2;
-        rownum++;
+        else
+        {
+            leult[i]=0;
+        }
+
     }
-    return voltSzomszed;
 }
 
-bool leultE(int leult[], int poz, int leulo)
+bool leultE(int leult[], int resztvevok_szama, int leulo)
 {
     bool mar_leult_e=false;
-    for(int i=0; i<=poz; i++)
+    for(int i=0; i<resztvevok_szama; i++)
     {
         if(leulo==leult[i])
         {
@@ -111,22 +179,18 @@ bool leultE(int leult[], int poz, int leulo)
     }
     return mar_leult_e;
 }
-void ulesi_sorrend(int resztvevok_szama, long long *szomszed, int *leult)
-{
-    for(int i=0; i<resztvevok_szama; i++)
-    {
-        szomszed[i]+=pow(2,leult[i]);
-        szomszed[leult[i]]+=pow(2,i);
-    }
-}
 
-void kiirVoltSzomszedE(int resztvevok_szama, long long *szomszed)
+void kiirVoltSzomszedE(int resztvevok_szama, int *szomszed)
 {
     for(int i=0; i<resztvevok_szama; i++)
     {
-        cout << szomszed[i]<< " ";
+        for(int j=0; j<resztvevok_szama; j++)
+        {
+            cout << szomszed[i*resztvevok_szama+j]<< " ";
+        }
+        cout << endl;
     }
-    cout << endl;
+
 }
 
 void kiirLeultE(int resztvevok_szama, int *leult)
